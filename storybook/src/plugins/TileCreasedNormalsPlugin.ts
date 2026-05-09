dc625672c8a94b63fcc7d93d49c2d2167407cfd7
Shota Matsuda <shota@takram.com>
2026-05-09 10:48:04 +0900

    Update TileCreasedNormalsPlugin in WebGL storybook

--- storybook/src/plugins/TileCreasedNormalsPlugin.ts
+++ storybook/src/plugins/TileCreasedNormalsPlugin.ts
@@ -1,24 +1,28 @@
-import type { Tile } from '3d-tiles-renderer'
-import { BufferGeometry, Mesh, type Object3D } from 'three'
-
+import type { Tile, TilesRenderer } from '3d-tiles-renderer'
 import {
-  fromBufferGeometryLike,
-  toBufferGeometryLike
-} from '@takram/three-geospatial'
+  BufferGeometry,
+  Mesh,
+  type BufferAttribute,
+  type Object3D
+} from 'three'
 
 import { queueTask } from '../worker/pool'
+import {
+  fromBufferAttributeLike,
+  toBufferAttributeLike
+} from '../worker/tasks/computeCreasedNormalAttribute'
 
-async function toCreasedNormalsAsync(
+async function computeCreasedNormalAttributeAsync(
   geometry: BufferGeometry,
   creaseAngle?: number
-): Promise<BufferGeometry> {
-  const [geometryLike, transfer] = toBufferGeometryLike(geometry)
+): Promise<BufferAttribute> {
+  const position = toBufferAttributeLike(geometry.getAttribute('position'))
   const result = await queueTask(
-    'toCreasedNormals',
-    [geometryLike, creaseAngle],
-    { transfer }
+    'computeCreasedNormalAttribute',
+    [position[0], creaseAngle],
+    { transfer: position[1] }
   )
-  return fromBufferGeometryLike(result, geometry)
+  return fromBufferAttributeLike(result)
 }
 
 export interface TileCreasedNormalsPluginOptions {
@@ -26,10 +30,21 @@ export interface TileCreasedNormalsPluginOptions {
 }
 
 export class TileCreasedNormalsPlugin {
+  tiles?: TilesRenderer
   readonly options: TileCreasedNormalsPluginOptions
+  priority = -1000
 
   constructor(options?: TileCreasedNormalsPluginOptions) {
     this.options = { ...options }
+  }
+
+  // Plugin method
+  init(tiles: TilesRenderer): void {
+    this.tiles = tiles
+
+    tiles.forEachLoadedModel((scene, tile) => {
+      void this.processTileModel(scene, tile)
+    })
   }
 
   // Plugin method
@@ -37,16 +52,22 @@ export class TileCreasedNormalsPlugin {
     const meshes: Mesh[] = []
     scene.traverse(object => {
       if (object instanceof Mesh && object.geometry instanceof BufferGeometry) {
+        // Synchronously convert to non-indexed geometries:
+        const { geometry } = object
+        if (geometry.index != null) {
+          object.geometry = geometry.toNonIndexed()
+          geometry.dispose()
+        }
         meshes.push(object)
       }
     })
     await Promise.all(
       meshes.map(async mesh => {
-        // eslint-disable-next-line require-atomic-updates
-        mesh.geometry = await toCreasedNormalsAsync(
+        const normal = await computeCreasedNormalAttributeAsync(
           mesh.geometry,
           this.options.creaseAngle
         )
+        mesh.geometry.setAttribute('normal', normal)
       })
     )
   }
